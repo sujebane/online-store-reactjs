35579063b49519363cdd9dca2f7925c45f092e48
Andrea Harris <aharris@mitrai.com>
2023-03-08 13:05:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Button from './components/Button';
+import NavbarElements from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavbarElements/>
+      <h1>OUR BUILDINGS INTEGRATE BEST COSY HOMES</h1>
+      <Button/>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./styles.css"
+export default function NavbarElements() {
+  return (
+    <nav>
+        <h1>LYSENA</h1>
+        <div>
+            <ul id="navbar">
+                <li><a href="index.html">HOME</a></li>
+                <li><a href="index.html">CONTACT US</a></li>
+            </ul>
+        </div>
+    </nav>
+    
+  )
+}
